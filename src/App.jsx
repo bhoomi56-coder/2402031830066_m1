67ec8fbb89b8b940c2d614323a2cf892ebabccba
Bhoomi Raval <bhoomiraval471@gmail.com>
2026-05-25 22:05:15 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,18 @@
-import { BrowserRouter, Routes, Route, Link ,useParams} from "react-router-dom";
+import { useState } from "react";
 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Products from "./Products";
-import Laptop from "./Laptop";
-import Phone from "./Phone";
+function Counter() {
+  const [count, setCount] = useState(0);
 
-function App() {
-  function User() {
-    console.log(useParams());
-    const { id } = useParams();
-    return <h2>User ID: {id}</h2>;
-  }
-  function NotFound() {
-    return <h2>404 Not Found</h2>;
+  function increase() {
+    setCount(count + 1);
   }
 
   return (
-    <BrowserRouter>
-      <h1>My React Router App</h1>
-        <nav>
-          <Link to="/">Home</Link> |
-           <Link to="/about">About</Link> |
-           <Link to="/contact">Contact</Link>|
-           <Link to="/user/10">User</Link>|
-           <Link to="/products">Products</Link>|
-        </nav>
-      
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/user/:id" element={<User />} />
-        <Route path="/products" element={<Products />}>
-          <Route path="laptops" element={<Laptop />} />
-          <Route path="phones" element={<Phone />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>Increase</button>
+    </div>
   );
 }
 
-export default App;
-
+export default Counter;
---
+++ src/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    function increase() {
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increase}>Increase</button>
+        </div>
+    );
+}
+
+export default Counter;
